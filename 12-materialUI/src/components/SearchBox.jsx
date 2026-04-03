606c634be6ebaf0feaaac00908d67c81482c161d
Supriya Kumari <sjha0885@example.com>
2026-04-03 19:31:09 +0530

    Fixed SearchBox error (useState import)

--- 12-materialUI/src/components/SearchBox.jsx
+++ 12-materialUI/src/components/SearchBox.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css"
